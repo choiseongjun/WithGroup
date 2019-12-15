46b735a5f88351e2ec627a8389ad4bbb0da9fa26
최성준 <csj2702@naver.com>
2019-12-15 14:21:08 +0900

    react-native vector icon 연동,axios로 모임리스트데이터불러옴

--- component/MainScreen.js
+++ component/MainScreen.js
@@ -1,25 +1,47 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet,FlatList} from 'react-native';
 import TabBar from './TabBar';
+import axios from 'axios';
+
 class MainScreen extends Component{
     constructor(props){
         super(props)
         this.state ={
-            type:''
+            type:'',
+            moims:[]
         }
         this.setType = this.setType.bind(this)
+    }
+
+    componentDidMount(){
+  
+    
+        axios.get(`http://52.79.57.173/rest/moimlistView`)
+        .then(res => {
+          const moims = res.data.moimList.content;
+          this.setState({ moims });
+        })
     }
     setType(type){
         this.setState({type})
       }
     render(){
+        const {moims} = this.state;
+   
+    console.log(moims);
         const{type} = this.state;
         return(
             <View style={styles.container}>
                 <View style={{flex:1}}>
-                  <Text>main</Text>
+                <FlatList
+                    data={moims}
+                renderItem={({item}) => <Text>타이틀:{item.title}:소개{item.intro}작성자:{item.people.name}</Text>
+                                            }
+                    />    
                 </View>
-                <TabBar type={type} setType={this.setType} />                             
+                                    
+                <TabBar type={type} setType={this.setType} />     
+                
            </View>
         );
     }
